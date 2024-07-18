eb142be729a55db1c74b50709fe79300451f6c7c
mspena14 <marlono1naranjo@gmail.com>
2024-07-18 15:22:57 -0500

    Final ejercicio 5

--- Ejercicio 5/app.js
+++ Ejercicio 5/app.js
@@ -1,18 +1,19 @@
+let timeoutTime = 1000;
+
 function manejarAsincronia(callback, promesa) {
-    promesa.then(() => {
-        callback("resuelto");
-    }).catch(() => {
-        callback("rechazado");
-    });
-}
+    promesa.then(() =>
+    callback("resuelto"))
+    .catch(() => 
+    callback("rechazado"));
+};
 
 function crearPromesa() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve();
-        }, 2000);
+        }, timeoutTime);
     });
-}
+};
 
 function miCallback(resultado) {
     if (resultado === "resuelto") {
@@ -20,7 +21,20 @@ function miCallback(resultado) {
     } else {
         console.log("¡Promesa rechazada y callback ejecutado!");
     }
-}
+};
 
 const promesa = crearPromesa();
 manejarAsincronia(miCallback, promesa);
+
+setTimeout(() => {
+    console.log(`5.5: Despues de invocar la funcion, responde las siguientes preguntas:
+    -¿Qué sucede si cambias el tiempo de resolución de la promesa a 5 segundos o a 1 segundo?
+    R// La función continuaría funcionando sin problema solo que tardaría más o menos tiempo.
+    
+    - ¿Cómo se comporta la función si la promesa es rechazada en lugar de resuelta?
+    R// La función ejecutaría el callback con un mensaje diferente según la promesa sea resuelta o rechazada, en este caso el catch recibiría el error e imprimiria el mensaje dejado y si no hay catch entonces este nos dará undefined y no ejecutará el callback.
+    
+    - ¿Puedes modificar la función para que el callback maneje diferentes tipos de información dependiendo del resultado de la promesa?
+    R//Sí, es posible y también es posible concatenar respuestas añadiendole procesos a seguir sí se obtiene un respuesta, como un paso a paso.
+    `); 
+}, timeoutTime);
