9c27f7d0cb98a26e43d81ad19258e8dcf73ffbe1
mspena14 <marlono1naranjo@gmail.com>
2024-07-18 20:31:30 -0500

    Inicio ejercicio 9

---
+++ Ejercicio 9/app.js
@@ -0,0 +1,6 @@
+/* - Crea un script interactivo que utilice setTimeout para ejecutar una acción después de un intervalo pedido al usuario.
+- Suponiendo que el usuario ingreso n segundos, el script debe mostrar un mensaje en consola despues de n\*1000 milisegundos.
+- Usa promesas para manejar la asincronía y mostrar el mensaje en consola.
+- Usa fetch para cargar datos de la siguiente URL: https://jsonplaceholder.typicode.com/posts
+- Muestra los datos en consola y maneja cualquier error que pueda ocurrir en el bloque catch de la promesa. */
+
