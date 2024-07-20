f71a756d0ce3e38add554532689b042260f3378e
mspena14 <marlono1naranjo@gmail.com>
2024-07-20 15:35:31 -0500

    Casi final ejercicio 10

---
+++ Ejercicio 10/app.js
@@ -0,0 +1,40 @@
+console.log("Inicio del script");
+
+// Macrotarea: setTimeout
+setTimeout(() => {
+  console.log("Macrotarea 1 second (setTimeout)");
+}, 1000);
+
+setTimeout(() => {
+  console.log("Macrotarea 0 seconds (setTimeout)");
+}, 0);
+
+// Microtarea: Promesa
+Promise.resolve()
+  .then(() => {
+    setTimeout(() => {
+      console.log("Macrotarea (setTimeout) inside Microtarea 1");
+      return "from micro 1";
+    }, 0);
+  })
+  .then((message) => {
+    console.log("Microtarea 2 (Promesa)");
+  });
+
+// Microtarea: Promesa
+Promise.resolve()
+  .then(() => {
+    console.log("Microtarea 3 (Promesa)");
+  })
+  .then(() => {
+    console.log("Microtarea 4 (Promesa)");
+  });
+
+console.log("Fin del script");
+
+Promise.resolve()
+  .then(()=>{
+    console.log(`¿Qué tareas se consideran macrotareas y cuáles son microtareas?
+R// Los set
+        `)
+  })
