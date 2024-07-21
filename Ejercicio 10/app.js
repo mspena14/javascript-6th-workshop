05b7883b20050cbd515260263e10ff76e63c90fc
mspena14 <marlono1naranjo@gmail.com>
2024-07-20 19:47:33 -0500

    Final punto 10

--- Ejercicio 10/app.js
+++ Ejercicio 10/app.js
@@ -33,8 +33,39 @@ Promise.resolve()
 console.log("Fin del script");
 
 Promise.resolve()
-  .then(()=>{
-    console.log(`¿Qué tareas se consideran macrotareas y cuáles son microtareas?
-R// Los set
-        `)
+  .then(() => {
+    setTimeout(() => {
+      console.log(`¿Qué sucede cuando una microtarea genera una nueva macrotarea dentro de ella?
+R// Esta macrotarea pasa a la fila de las macrotareas, no se ejecuta inmediatamente, se agregará normalmente a la fila de tareas en la última posición. 
+                                                                                        `);
+    }, 1000);
   })
+  .then(() => {
+    setTimeout(() => {
+      console.log(`¿Cómo se manejan las promesas y los setTimeout en relación con el event loop?
+R// Las promesas al ser microtareas el event loop le asigna un lugar en la cola de microtareas,
+la cual está aparte de la cola de los setTimeOut que son las macrotareas y las ambas a su vez están aparte
+de la sección de ejecución que pasa directamente a los procesos sincronos, luego pasa a ejecutar las microtareas,
+que solo hasta que se termine la última microtarea se pasa a ejecutar la primera macrotarea y al terminar esta,
+se inicia de nuevo el ciclo, si no hay microtareas por ejecuar, se ejecutan la siguiente macrotarea.
+                                    `);
+    }, 1000);
+  })
+
+setTimeout(() => {
+  console.log(`¿Qué tareas se consideran macrotareas y cuáles son microtareas?
+R// Las macrotareas son las tareas de mayor prioridad que incluye setTimeOut o eventos del DOM, 
+por otro lado las microtareas don de menor prioridad que normalmente se relacionan con las promesas.  
+                                                                `);
+}, 1000);
+
+setTimeout(() => {
+  console.log(`¿Cómo se relacionan las macrotareas y microtareas con el event loop?
+R// Estas se relacionan en que ambas deben respetar un orden, en que se ejecutan una después de la otra en su propia fila,
+el event loop las administra, ejecuta cada fila en un orden especifico y luego empieza de nuevo el ciclo de la siguiente manera,
+1- Tareas sincronas.
+2- Microtareas.
+3- Macrotareas.
+4- Se repite el ciclo.
+                                  `);
+}, 1000);
